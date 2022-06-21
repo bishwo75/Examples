d68e09a5a2b579bac10c104006fafbcaddef19f4
bishwo75 <54118149+bishwo75@users.noreply.github.com>
2022-06-21 09:01:45 -0700

    Add files via upload

---
+++ mapDelete.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+//import { useState } from 'react/cjs/react.development';
+
+const players112 = ["Ram", "Shyam", "Bash", "Akesha", "Surya", "Rajendra", "Ram"] 
+const MapDelete = (props) => {
+    //const {mapDelete} = props
+    // const [players, setPlayers] = useState(mapDelete)
+    const [players, setPlayers] = useState(players112)
+  //Ask dallas, why we need to have on line 10 as const instead of handleClick only as on line 25, didn't we call as a function on line 25? got questions
+    const handleClick= (index) => {
+      const newPlayers = [...players];
+      newPlayers.splice(index, 1)
+      setPlayers(newPlayers)
+    }
+
+    const resetClick = () => {
+      setPlayers(players112)
+    }
+    return ( 
+        <div>
+            <ul>
+                <p>Test here with Hooks for the state ones</p>
+              {players.map((abc, index) => {
+                  return (
+                  <li key={Math.random.toString()} onClick={() => handleClick(index)}>{abc}</li>                  
+                  )                 
+              })}  
+               <p>There are {players.length} players.</p>
+               {players.length < players112.length && <button onClick={()=> resetClick()}>Reset</button>}
+            </ul>
+        </div>
+     );
+}
+ 
+export default MapDelete;
+
+
