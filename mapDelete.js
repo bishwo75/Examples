e478085eba7d4984f44b425f5ffc44158c84bfc1
bishwo75 <54118149+bishwo75@users.noreply.github.com>
2022-08-09 19:29:38 -0400

    Update mapDelete.js

--- mapDelete.js
+++ mapDelete.js
@@ -31,3 +31,33 @@ const MapDelete = (props) => {
 export default MapDelete;
 
 
+// const players112 = ["Ram", "Shyam", "Bash", "Akesha", "Surya", "Rajendra", "Ram"] 
+// const MapDelete = () => {    
+//     const [players, setPlayers] = useState(players112)  
+//     const handleClick= (abc) => {
+//       const newPlayers = players.filter((item) => item !== abc)
+//       setPlayers(newPlayers)
+//     }
+
+//     const resetClick = () => {
+//       setPlayers(players112)
+//     }
+//     return ( 
+//         <div>
+//             <ul>
+//                 <p>Test here with Hooks for the state ones</p>
+//               {players.map((abc) => {
+//                   return (
+//                   <li key={Math.random.toString()} onClick={() => handleClick(abc)}>{abc}</li>                  
+//                   )                 
+//               })}  
+//                <p>There are {players.length} players.</p>
+//                {players.length < players112.length && <button onClick={()=> resetClick()}>Reset</button>}
+//             </ul>
+//         </div>
+//      );
+// }
+ 
+// export default MapDelete;
+
+
