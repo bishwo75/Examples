134caf1e7f31eb269da1a88f74eaa08bcbf480fc
bishwo75 <54118149+bishwo75@users.noreply.github.com>
2023-12-02 19:29:40 -0800

    Update formReset.js

--- formReset.js
+++ formReset.js
@@ -21,6 +21,55 @@ const MapDelete = () => {
 }
  
 export default MapDelete;
+// 2nd Dec 2023 !!
+//Also, if you do not call the setState near the input field, the below is the way to do on onChange when calling a method:
+// const Tailwind = () => {
+//   const [name, setName] = useState({
+//     firstName: "",
+//     lastName: "",
+//   });
+
+//     const handleChange = (e, inputName) => {
+//         setName({
+//             ...name, 
+//             [inputName] : e.target.value})
+//     }
+//   return (
+//     <div className="font-bold text-color-green">
+     
+//       <div className="align-middle">
+//         <input
+//           type="text"
+//           placeholder="First Name"
+//           value={name.firstName}
+//         //   onChange={(e) => setName({ ...name, firstName: e.target.value })}
+//         onChange={(e)=>handleChange(e, "firstName")}
+//         />
+//         &nbsp;
+//         <input
+//           type="text"
+//           placeholder="Last Name"
+//           value={name.lastName}
+//         //   onChange={(e) => setName({ ...name, lastName: e.target.value })}
+//         onChange={(e)=>handleChange(e, "lastName")}
+//         />
+//         &nbsp;
+//         <p>
+//           {name.firstName} &nbsp; {name.lastName}
+//         </p>
+//         {(name.firstName.length>0 || name.lastName.length>0) &&
+//         <button onClick={()=>setName({firstName:'', lastName:''})}>Reset</button>
+//         }
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Tailwind;
+
+
+
+
 //Also, if we want to show the reset button once both first name and last name input field are filled, then we can write like this:
 // {name.firstName.length > 0 && name.lastName.length >0  && 
 //     <button onClick={()=>setName({firstName:'', lastName:''})}>Reset</button>
